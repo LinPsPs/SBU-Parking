77af9efac14ec75f95db2a24a03d6807bb29b7cd
Shengteng Ouyang <shengtengouyang@gmail.com>
2020-09-26 12:15:49 -0400

    edit view and connect to server to calculate car count

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import {Avatar, Button} from '@fluentui/react-northstar';
 import axios from 'axios' ;
+import map from './images/map.png'
 
 import './assets/css/font-awesome.min.css'
 import './assets/css/main.css'
@@ -55,72 +56,79 @@ import './assets/css/main.css'
 class App extends Component {
 
   state = {
-    total: 10,
-    num_of_cars: [1, 1, 1, 1, 1, 1]
+	  cars:{0:10, 1:10, 2:10, 3:10, 4:10, 5:10, 6:10, 7:10}
+  }
+
+  handleProcess=(parkingLot)=>{
+	// alert("hahaha");
+	let  url="http://127.0.0.1:5000/"+parkingLot
+  	axios.get(url)
+    .then(function (response) {
+	  let data =response.data
+	  this.setState(state=>({
+		  ...state,
+		  cars:{...this.state.cars, [parkingLot]: data}
+		}));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
   render() {
     return (
       <div id="wrapper">
 
-					<header id="header">
-						{/* <span class="avatar"><img src={avatar} alt="" /></span> */}
-						<h1>This is <strong>SBU Parking</strong></h1>
-						{/* <ul class="icons">
-							<li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
-							<li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
-							<li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
-							<li><a href="#" class="icon style2 fa-500px"><span class="label">500px</span></a></li>
-							<li><a href="#" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
-						</ul> */}
-					</header>
+			<header id="header">
+				{/* <span class="avatar"><img src={avatar} alt="" /></span> */}
+				<h1>This is <strong>SBU Parking</strong></h1>
+			</header>
+			<aside className="left">
+					<img src={map} className="photo"/>
+			</aside>
+			<section className="right">
+				<section class="thumbnails">
+					<div>
+						<a onClick={()=>this.handleProcess(1)}>
+							<img src="images/thumbs/01.jpg" alt=""/>
+							<h3>{this.state.cars[1]}</h3>
+						</a>
+						<a onClick={()=>this.handleProcess(2)}>
+							<img src="images/thumbs/02.jpg" alt=""/>
+							<h3>{this.state.cars[2]}</h3>
+						</a>
+					</div>
+					<div>
+						<a onClick={()=>this.handleProcess(3)}>
+							<img src="images/thumbs/03.jpg" alt=""/>
+							<h3>{this.state.cars[3]}</h3>
+						</a>
+						<a onClick={()=>this.handleProcess(4)}>
+							<img src="images/thumbs/04.jpg" alt=""/>
+							<h3>{this.state.cars[4]}</h3>
+						</a>
+						<a onClick={()=>this.handleProcess(5)}>
+							<img src="images/thumbs/05.jpg" alt=""/>
+							<h3>{this.state.cars[5]}</h3>
+						</a>
+					</div>
+					<div>
+						<a onClick={()=>this.handleProcess(6)}>
+							<img src="images/thumbs/06.jpg" alt=""/>
+							<h3>{this.state.cars[6]}</h3>
+						</a>
+						<a onClick={()=>this.handleProcess(7)}>
+							<img src="images/thumbs/07.jpg" alt=""/>
+							<h3>{this.state.cars[7]}</h3>
+						</a>
+					</div>
+				</section>
+				<footer id="footer">
+					<p>&copy; SBU Parking</p>
+				</footer>
+			</section>
 
-					<section id="main">
-
-							<section class="thumbnails">
-								<div>
-									<a>
-										<img src="images/thumbs/01.jpg" alt="" />
-										<h3>Lorem ipsum dolor sit amet</h3>
-									</a>
-									<a>
-										<img src="images/thumbs/02.jpg" alt="" />
-										<h3>Lorem ipsum dolor sit amet</h3>
-									</a>
-								</div>
-								<div>
-									<a>
-										<img src="images/thumbs/03.jpg" alt="" />
-										<h3>Lorem ipsum dolor sit amet</h3>
-									</a>
-									<a>
-										<img src="images/thumbs/04.jpg" alt="" />
-										<h3>Lorem ipsum dolor sit amet</h3>
-									</a>
-									<a>
-										<img src="images/thumbs/05.jpg" alt="" />
-										<h3>Lorem ipsum dolor sit amet</h3>
-									</a>
-								</div>
-								<div>
-									<a>
-										<img src="images/thumbs/06.jpg" alt="" />
-										<h3>Lorem ipsum dolor sit amet</h3>
-									</a>
-									<a>
-										<img src="images/thumbs/07.jpg" alt="" />
-										<h3>Lorem ipsum dolor sit amet</h3>
-									</a>
-								</div>
-							</section>
-
-					</section>
-
-					<footer id="footer">
-						<p>&copy; SBU Parking</p>
-					</footer>
-
-			</div>
+		</div>
     )
   }
 }
